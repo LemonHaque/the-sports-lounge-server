706101f40b95b142899c2e95e871dc2f09347fcd
LemonHaque <azizulhaquelemon31@gmail.com>
2023-06-09 15:05:30 +0600

    initial setup done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express());
+
+
+app.get('/', (req, res) => {
+    res.send('sports is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`Sports Lounge is runnig on port ${port}`);
+})
