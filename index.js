27a01d1897a9562a360f8226e490ed24b948149f
LemonHaque <azizulhaquelemon31@gmail.com>
2023-06-11 10:56:48 +0600

    added googleSign in user

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ async function run() {
         // users related api
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User Already exists' })
+            }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
