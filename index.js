612a8f3c888e3a2320a087a3cdc0c417b50c0876
LemonHaque <azizulhaquelemon31@gmail.com>
2023-06-11 17:48:06 +0600

    added verify admin

--- index.js
+++ index.js
@@ -57,8 +57,19 @@ async function run() {
             res.send({ token })
         })
 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        }
+
+
         // users related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
